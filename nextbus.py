5ffdd2586ef2025953bc2ce32e12b48d5dd7f2de
nlovlyn <nlovlyn@gmail.com>
2016-02-09 17:14:51 +0800

    Debug

--- nextbus.py
+++ nextbus.py
@@ -1,7 +1,6 @@
 import lcddriver
 import time
-#from predict import predict
-import predict
+from predict import predict
 import threading
 
 lcd = lcddriver.lcd()
@@ -26,30 +25,65 @@ for s in stops:
 
 time.sleep(1) # Allow a moment for initial results
 
+# Define stops
+HollisEMY = predictList[0].data[1]
+HollisBART = predictList[1].data[1]
+Powell = predictList[2].data[1]
+J = predictList[3].data[1]
+F = predictList[4].data[1]
+
+# Define predictions1
+HEP0 = 0
+HE0S = 0
+HEP1 = 0
+HE1S = 0
+HBP0 = 0
+HB0S = 0
+HBP1 = 0
+HB1S = 0
+PP0 = 0
+P0S = 0
+PP1 = 0
+P1S = 0
+JP0 = 0
+J0S = 0
+JP1 = 0
+J1S = 0
+FP0 = 0
+F0S = 0
+FP1 = 0
+F1S = 0
+
 while True:
 	currentTime = time.time()
-	print predictList[0].data[1]
-	print predictList[0].predictions[0]
-	a = predictList[0].data[1]
-	b =  predictList[0].predictions[0]
-	lcd.lcd_display_string(a, 3)
-	lcd.lcd_display_string(b, 4)
-	#lcd - first line 
-	for pl in predictList:
-		time.sleep(.2)
+	# wait for array to be filled before attempting to access
+	if len(predictList) > 0 and predictList[0].predictions:
+		print predictList[0].predictions[0]
+		HE0S = predictList[0].predictions[0]
+		
+		
+		lcd.lcd_display_string(b, 4)
+	else:
+		x = HollisEMY + ": No Data"
+		lcd.lcd_display_string(x, 4)     
+
+	#lcd - first line
+	# 
+	#for pl in predictList:
+		#time.sleep(.2)
 		#print pl.data[1] + ' ' + pl.data[3] + ':'
 		#print pl.data[3]
 		#lcd.lcd_display_string(pl.data[3], 4)
-		if pl.predictions: # List of arrival times, in seconds
-					for p in pl.predictions:
+		#if pl.predictions: # List of arrival times, in seconds
+					#for p in pl.predictions:
 						# Extrapolate from predicted arrival time,
 						# current time and time of last query,
 						# display in whole minutes.
-						t = p - (currentTime - pl.lastQueryTime)
+						#t = p - (currentTime - pl.lastQueryTime)
 						#print '\t' + str(int(t/60)) + ' min'
 						#lcd.lcd_display_string(t, 4)
-		else:
+		#else:
 					#print '\tNo predictions'
 					#lcd.lcd_display_string("No predictions", 4)
-					prevTime = currentTime;
+					#prevTime = currentTime;
 	time.sleep(5) # Refresh every ~5 seconds
